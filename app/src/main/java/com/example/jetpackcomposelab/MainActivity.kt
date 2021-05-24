80a3c106ba20c72355df1e6f5e75f5abdb263a78
Wagner Paulino <wagnersantos9778@gmail.com>
2021-05-24 20:15:45 -0300

    initial commit

---
+++ app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.jetpackcomposelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            newStory()
+        }
+    }
+
+    @Composable
+    fun newStory() {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        newStory()
+    }
+}
