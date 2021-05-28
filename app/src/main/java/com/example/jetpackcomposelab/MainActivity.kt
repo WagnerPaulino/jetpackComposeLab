aabbb9be02f6d9fef302e3e34db032da6741d581
Wagner Paulino <wagnersantos9778@gmail.com>
2021-05-27 21:20:24 -0300

    counter click

--- app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
@@ -3,34 +3,32 @@ package com.example.jetpackcomposelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            newStory()
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+                ClickCounter()
+            }
         }
     }
 
     @Composable
-    fun newStory() {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("A day in Shark Fin Cove")
-            Text("Davenport, California")
-            Text("December 2018")
+    fun ClickCounter() {
+        val count = remember { mutableStateOf(0) }
+        Text("I've been clicked ${count.value} times")
+        Button(onClick = { count.value++ }) {
+            Text("Click me")
         }
-    }
-
-    @Preview
-    @Composable
-    fun PreviewGreeting() {
-        newStory()
     }
 }
