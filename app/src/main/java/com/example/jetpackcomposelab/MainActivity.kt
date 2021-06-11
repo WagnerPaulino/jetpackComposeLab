4644ca9ac4a6f2bfd5046d61a20b3a67ab3656f7
Wagner Paulino <wagnersantos9778@gmail.com>
2021-06-10 22:05:02 -0300

    passando o state atraves dos composes

--- app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
@@ -3,32 +3,50 @@ package com.example.jetpackcomposelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-                ClickCounter()
-            }
+            MyScreenContent()
         }
     }
 
+    @Preview
     @Composable
-    fun ClickCounter() {
+    fun MyScreenContent() {
         val count = remember { mutableStateOf(0) }
-        Text("I've been clicked ${count.value} times")
-        Button(onClick = { count.value++ }) {
+        Column(modifier = Modifier.fillMaxHeight()) {
+            Column(modifier = Modifier.weight(1f)) {
+                    Text("Hello")
+                    Divider(color = Color.Black)
+                }
+            ClickCounter(
+                count = count.value,
+                updateCount = { newCount ->
+                    count.value = newCount
+                }
+            )
+            }
+        }
+
+    @Composable
+    fun ClickCounter(count: Int, updateCount: (Int) -> Unit) {
+        Text("I've been clicked $count times")
+        Button(onClick = { updateCount(count+1) }) {
             Text("Click me")
         }
     }
-}
+
+    }
