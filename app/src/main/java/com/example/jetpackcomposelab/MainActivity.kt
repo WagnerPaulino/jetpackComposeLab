7f890cecdea7a552142838c92024b490aa1a8c3c
Wagner Paulino <wagnersantos9778@gmail.com>
2021-06-15 21:00:11 -0300

    .

--- app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -14,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,21 +29,33 @@ class MainActivity : ComponentActivity() {
 
     @Preview
     @Composable
-    fun MyScreenContent() {
+    fun MyScreenContent(names: List<String> = List(100) { "Hello Android #$it" }) {
         val count = remember { mutableStateOf(0) }
         Column(modifier = Modifier.fillMaxHeight()) {
-            Column(modifier = Modifier.weight(1f)) {
-                    Text("Hello")
-                    Divider(color = Color.Black)
-                }
+            NameList(names, Modifier.weight(1f))
             ClickCounter(
                 count = count.value,
                 updateCount = { newCount ->
                     count.value = newCount
                 }
             )
+        }
+    }
+
+    @Composable
+    fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(names) { name ->
+                Greeting(name = name)
+                Divider(color = Color.Black)
             }
         }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 
     @Composable
     fun ClickCounter(count: Int, updateCount: (Int) -> Unit) {
@@ -49,4 +65,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    }
+}
