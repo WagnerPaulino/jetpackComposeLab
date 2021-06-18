a9bab0a7d0488231cfe0e574dbe3111d0862793d
Wagner Paulino <wagnersantos9778@gmail.com>
2021-06-17 22:05:39 -0300

    brincando com states

--- app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelab/MainActivity.kt
@@ -3,17 +3,18 @@ package com.example.jetpackcomposelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,17 +45,27 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+        val markeds by remember { mutableStateOf(MutableList(names.size) { false }) }
         LazyColumn(modifier = modifier) {
-            items(names) { name ->
-                Greeting(name = name)
+            itemsIndexed(names) { index, name ->
+                Greeting(name = name, markeds[index]) {value -> markeds[index] = value}
                 Divider(color = Color.Black)
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    fun Greeting(name: String, value: Boolean, onClick: (value: Boolean) -> Unit) {
+        var isSelected by remember { mutableStateOf(value) }
+        val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+        Text(text = "Hello $name!", modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable {
+                isSelected = !isSelected
+                onClick(isSelected)
+            }
+        )
     }
 
     @Composable
